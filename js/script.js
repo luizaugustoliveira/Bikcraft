b3a773cff0416ba31104b75ffc1d5616c10365d2
Luiz Augusto Oliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2022-09-21 15:50:03 -0300

    implementacao de segeuros

--- js/script.js
+++ js/script.js
@@ -1,35 +1,62 @@
-const params = new URLSearchParams(location.search);
-
-params.forEach((item) => {
-  const element = document.querySelector(`[type="radio"][value="${item}"]`);
-  if (element) element.checked = true;
-});
-
-const dts = document.querySelectorAll("dt");
-
-dts.forEach((dt) => {
-  dt.addEventListener("click", () => {
-    dt.parentElement.classList.toggle("ativa");
-  });
-});
-
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
-links.forEach((link) => {
-  if (location.href.includes(link.href)) {
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
     link.classList.add("ativo");
   }
-});
+}
 
+links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
 const galeria = document.querySelectorAll(".bicicleta-imagens img");
 const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
-galeria.forEach((img) => {
-  img.addEventListener("click", () => {
-    if (matchMedia("(min-width: 1000px)").matches) {
-      galeriaContainer.prepend(img);
-    }
-  });
-});
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
 
-// if (window.SimpleAnime) new SimpleAnime();
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
